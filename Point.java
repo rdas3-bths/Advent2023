ab41483a9424cfcb45ecad96fb16bab69a598094
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2023-12-18 16:09:45 -0500

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,29 @@
+public class Point {
+    private long x;
+    private long y;
+
+    public Point(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
